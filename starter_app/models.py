37b417b1f5be78cff1723ec3c487a264262f0339
Matt Zamora <mzamora2899@gmail.com>
2020-10-25 17:09:03 -0600

    create models and relationships between them

--- starter_app/models.py
+++ starter_app/models.py
@@ -1,17 +1,71 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
+favorites = db.Table('favorites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('restaurant_id', db.Integer, db.ForeignKey('restaurant.id'), primary_key=True)
+)
+
+
 class User(db.Model):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    reservations = db.relationship('Reservation', backref='user', lazy=True)
+    reviews = db.relationship('Review', backref='user', lazy=True)
+    favorites = db.relationship('Restaurant', secondary=favorites, lazy='subquery', backref=db.backref('users', lazy=True))
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "city": self.city,
+            "state": self.state,
+            "password": self.password
+        }
+
+
+class Reservation(db.Model):
+
+    __tablename__ = 'reservations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False, db.ForeignKey('user.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    group_num = db.Column(db.Numeric(1, 10), nullable=False)
+    start_time = db.Column(db.DateTime(timezone=True), nullable=False)
+
+
+class Review(db.Model):
+
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Numeric(1, 5), nullable=False)
+
+
+class Restaurant(db.Model):
+
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    avg_rating = db.Column(db.Float, nullable=True)
+    min_price = db.Column(db.Integer, nullable=True)
+    max_price = db.Column(db.Integer, nullable=True)
+    reviews = db.relationship('Review', backref='restaurant', lazy=True)
+    reservations = db.relationship('Reservation', backref='restaurant', lazy=True)
