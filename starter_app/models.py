67d11ac9b396160197113b3f4ebbdd8d6c43f679
Yongho Kim <winthisgame12@gmail.com>
2020-10-26 09:49:31 -0600

    add models.py

---
+++ starter_app/models.py
@@ -0,0 +1,69 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+favorites = db.Table('favorites',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('restaurant_id', db.Integer, db.ForeignKey('restaurants.id'), primary_key=True)
+)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    hashed_password = db.Column(db.String(100), nullable=False)
+    reservations = db.relationship('Reservation', backref='user', lazy=True)
+    reviews = db.relationship('Review', backref='user', lazy=True)
+    favorites = db.relationship('Restaurant', secondary=favorites, lazy='subquery', backref=db.backref('users', lazy=True))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "city": self.city,
+            "state": self.state,
+            "password": self.password
+        }
+
+
+class Restaurant(db.Model):
+
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    avg_rating = db.Column(db.Float, nullable=True)
+    min_price = db.Column(db.Integer, nullable=True)
+    max_price = db.Column(db.Integer, nullable=True)
+    reviews = db.relationship('Review', backref='restaurant', lazy=True)
+    reservations = db.relationship('Reservation', backref='restaurant', lazy=True)
+
+class Reservation(db.Model):
+
+    __tablename__ = 'reservations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    group_num = db.Column(db.Integer, nullable=False)
+    start_time = db.Column(db.DateTime(timezone=True), nullable=False)
+
+
+class Review(db.Model):
+
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
