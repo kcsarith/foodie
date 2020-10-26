24659083354d77f416adec7cc8aa21173b7cf0f7
Matt Zamora <mzamora2899@gmail.com>
2020-10-26 14:34:51 -0600

    fix compnents

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,64 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, useLocation, Switch, Route, NavLink, Redirect } from 'react-router-dom';
+import { useLocation, Switch, Route, NavLink, Redirect } from 'react-router-dom';
 import LoginPanel from './components/LoginPanel';
 //import NavBar from './components/NavBar';
 import Profile from './components/Profile';
 import UserList from './components/UsersList';
 import { getUserInfo } from './store/currentUser';
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  let needLogin = useSelector(state => !state.authentication.id);
-  return (
-      <Route {...rest} render={(props) => (
-          needLogin
-          ? <Redirect to='/login' />
-          : <Component {...props} />
-      )} />
-  )
-}
+// const PrivateRoute = ({ component: Component, ...rest }) => {
+//     let needLogin = useSelector(state => !state.authentication.id);
+//     return (
+//         <Route {...rest} render={(props) => (
+//             needLogin
+//                 ? <Redirect to='/login' />
+//                 : <Component {...props} />
+//         )} />
+//     )
+// }
 
 function App() {
-  let currentUserId = useSelector(state => state.authentication.id);
-  //let location = useLocation();
-  let dispatch = useDispatch();
+    let currentUserId = useSelector(state => state.authentication.id);
+    //let location = useLocation();
+    let dispatch = useDispatch();
 
-  // useEffect(() => {
-  //     dispatch(getUserInfo(currentUserId));
-  // }, [currentUserId, dispatch])
+    // useEffect(() => {
+    //     dispatch(getUserInfo(currentUserId));
+    // }, [currentUserId, dispatch])
 
-
-  return (
-    <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-            </ul>
-        </nav>
-        {/* {location.pathname !== '/login' && location.pathname !== '/signup' ?
+    // if (!currentUserId) {
+    //     return <Redirect to='/login' />
+    // }
+    return (
+        <>
+            {/* {location.pathname !== '/login' && location.pathname !== '/signup' ?
             <NavBar />
             : null} */}
-        <Switch>
-            <Route path="/login" component={LoginPanel} />
-            <PrivateRoute
-                path="/users"
-                exact={true}
-                component={UserList}
-            />
-            <PrivateRoute
-                path="/profile"
-                exact={true}
-                component={Profile}
-            />
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
-        </Switch>
-    </BrowserRouter>
-  );
+            <Switch>
+                <Route exact path="/login" component={LoginPanel} />
+                <Route
+                    path="/users"
+                    exact={true}
+                    component={UserList}
+                />
+                <Route
+                    path="/profile"
+                    exact={true}
+                    component={Profile}
+                />
+                <Route exact path="/">
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/" activeclass="active">Home</NavLink></li>
+                            <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
+                        </ul>
+                    </nav>
+                    <h1>My Home Page</h1>
+                </Route>
+            </Switch>
+        </>
+    );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import { BrowserRouter } from 'react-router-dom'
 
 const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
