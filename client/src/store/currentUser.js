13ced6dfcff2bacddd41172178871d84721c2609
Krisna Sarith <krisnasarith@gmail.com>
2020-10-27 15:13:11 -0700

    profile start

--- client/src/store/currentUser.js
+++ client/src/store/currentUser.js
@@ -1,8 +1,16 @@
 const SET_USER = 'FOODIE/CUURENTUSER/SET_USER';
+const UPDATE_USER = 'FOODIE/CURRENTUSER/UPDATE_USER'
 
 export const setUser = user => {
   return {
     type: SET_USER,
+    user
+  }
+}
+
+export const updateUser = user => {
+  return {
+    type: UPDATE_USER,
     user
   }
 }
@@ -20,9 +28,24 @@ export const getUserInfo = (id) => {
   }
 }
 
-export default function reducer(state = {}, action){
-  switch(action.type){
+export const updateUserInfo = (id) => {
+  return async dispatch => {
+    const response = await fetch(`/api/users/${id}/update`);
+    if (response.ok) {
+      const user = await response.json();
+      dispatch(updateUser(user));
+    } else {
+      const error = await response.json();
+      window.alert(error.message);
+    }
+  }
+}
+
+export default function reducer(state = {}, action) {
+  switch (action.type) {
     case SET_USER:
+      return action.user;
+    case UPDATE_USER:
       return action.user;
     default:
       return state;
--- starter_app/api/user_routes.py
+++ starter_app/api/user_routes.py
@@ -8,3 +8,25 @@ user_routes = Blueprint('users', __name__)
 def index():
     response = User.query.all()
     return {"users": [user.to_dict() for user in response]}
+
+# Krisna
+@user_routes.route("/:id/update", methods=["GET", "PUT"])
+def update():
+    id = request.args.get('id')
+    user = User.query.get(id)
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+    email = request.json.get("email", None)
+    user = User.query.filter(User.email == email).first()
+    print(user)
+    if user:
+        # return {"errors":["The email you've entered has been already registed"]}, 400
+         return jsonify({"msg": "The email you've entered has been already registed"}), 400
+    name = request.json.get("name", None)
+    password = request.json.get("password", None)
+    city = request.json.get("city", None)
+    state = request.json.get("state", None)
+    newUser = User(name=name, email=email, password=password, city=city, state=state)
+    db.session.add(newUser)
+    db.session.commit()
+    return {"user": "user" }
