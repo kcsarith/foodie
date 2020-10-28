176b1464f2c37e7ea73ab1bdac95b7e1a1b9f08f
xxl4tomxu98 <tomxu@udel.edu>
2020-10-27 20:58:03 -0400

    W20D2-merge

--- starter_app/api/home.py
+++ starter_app/api/home.py
@@ -11,9 +11,10 @@ def index(id):
     rest_list = Restaurant.query.filter_by(city=user_rest['city']).all()
     return {'restaurants': [rest.to_dict() for rest in rest_list]}
 
+
 @bp.route('/restaurant/<int:rest_id>')
 def reviews(rest_id):
 
     response = Review.query.filter_by(id=rest_id).all()
 
-    return { 'reviews': [review.to_dict() for review in response]}
+    return {'reviews': [review.to_dict() for review in response]}
